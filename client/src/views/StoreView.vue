2b0f4df0e3a0635b9c34be3d410c8a30a1cb7c1e
kgreen2022 <greenk7@newpaltz.edu>
2022-10-14 10:50:58 -0400

    Made adjustments to catch up

---
+++ client/src/views/StoreView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <h1 class="title">Store View</h1>
+        <p>Store View</p>
+    </div>
+</template>
+<style scoped>
+
+</style>
