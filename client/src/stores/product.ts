9a6f21ac7798cb16737327b6781e1aa250d114c6
kgreen2022 <greenk7@newpaltz.edu>
2022-10-24 15:16:12 -0400

    Some work during class 10.24

---
+++ client/src/stores/product.ts
@@ -0,0 +1,12 @@
+import data from '..data/products.json';
+export function getProducts() {
+    return data.products;
+}
+
+export function getProduct(id: number){
+    return data.products.find(product) =>  product.id== id;
+}
+
+export function deleteProduct(id: number){
+    data.product = data.products.filter(product) =>  product.id== id;
+}
---
+++ client/src/views/ProductsDetails.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import { reactive } from "vue";
+    import { getProducts } from "../stores/products";
+
+    const products = reactive(getProducts());
+</script>
+<template>
+    <div>
+        <div class="product" v-for="product in products" :key="product.id">
+            <img :src="product.thumbnail" :all="product.title" />
+        </div>
+        <div class="product.info">
+
+        </div>
+    </div>
+</template>
+<style scoped>
+    .products {
+        display: flex;
+        flex-wrap: wrap;
+        background-color: blue;
+    }
+    .product{
+        flex-basis: 10px;
+        margin: 1em;
+        border: 1px solid black;
+        border-radius: 
+
+    }
+
+</style>
