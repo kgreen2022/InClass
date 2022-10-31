d84eb3421044edc63e360f8137547df7554897c0
kgreen2022 <greenk7@newpaltz.edu>
2022-10-31 13:39:34 -0400

    Finish updating from class 10.27

---
+++ client/src/views/Cart.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const cart = {
+        
+    }
+</script>
+
+<template>
+    <div class="fly-out">
+        <h3 class="title is-3">Cart</h3>
+        <button class="button is-danger" @click="closeCart">Close</button>
+    </div>
+    <div class="fly-out-body">
+
+    </div>
+</template>
+
+<style scoped>
+    .fly-out {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        width: 20mm;
+        background-color: aliceblue;
+        border-left: 1px solid black;
+
+    }
+</style>
